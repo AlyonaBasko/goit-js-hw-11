5f92941389703125c8596552043123447973dc00
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-04-05 21:49:33 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import { searchImages } from './js/pixabay-api.js';
+import { renderImages } from './js/render-functions.js';
+
+const searchForm = document.getElementById('search-form');
+
+searchForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    const searchInput = document.getElementById('search-input');
+    const query = searchInput.value.trim();
+    
+    if (query === '') {
+        iziToast.warning({
+            title: 'Warning!',
+            message: 'Please enter a search query',
+            position: 'topCenter'
+        });
+        return;
+    }
+    
+    try {
+        const images = await searchImages(query);
+        renderImages(images);
+    } catch (error) {
+        console.error('Error searching images:', error.message);
+    }
+});
