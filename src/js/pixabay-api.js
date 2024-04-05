4db71c8d38e678776e5dd52ebfb8410d5c892865
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-04-05 21:24:23 +0200

    Update pixabay-api.js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+
+const API_KEY = '43249978-ed8444d52eae2c923645cb9a6';
+
+export async function searchImages(query) {
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch images');
+        }
+        const data = await response.json();
+        return data.hits;
+    } catch (error) {
+        console.error('Error searching images:', error.message);
+        throw error;
+    }
+}
