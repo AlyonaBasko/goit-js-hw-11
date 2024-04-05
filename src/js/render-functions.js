ef14067df3f08d7a2ea9d56c49d9a626fef0a76f
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-04-05 21:49:27 +0200

    Update render-functions.js

---
+++ src/js/render-functions.js
@@ -0,0 +1,53 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+export function renderImages(images) {
+    const gallery = document.getElementById('gallery');
+    gallery.innerHTML = '';
+
+    if (images.length === 0) {
+        showNoImagesMessage();
+        return;
+    }
+    images.forEach(image => {
+        const card = createImageCard(image);
+        gallery.appendChild(card);
+    });
+}
+
+function createImageCard(image) {
+    const card = document.createElement('div');
+    card.classList.add('image-card');
+
+    const img = document.createElement('img');
+    img.src = image.webformatURL;
+    img.alt = image.tags;
+
+    const likes = document.createElement('p');
+    likes.textContent = `Likes: ${image.likes}`;
+
+    const views = document.createElement('p');
+    views.textContent = `Views: ${image.views}`;
+
+    const comments = document.createElement('p');
+    comments.textContent = `Comments: ${image.comments}`;
+
+    const downloads = document.createElement('p');
+    downloads.textContent = `Downloads: ${image.downloads}`;
+
+    card.appendChild(img);
+    card.appendChild(likes);
+    card.appendChild(views);
+    card.appendChild(comments);
+    card.appendChild(downloads);
+
+    return card;
+}
+
+function showNoImagesMessage() {
+    iziToast.error({
+        title: 'Sorry!',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight'
+    });
+}
